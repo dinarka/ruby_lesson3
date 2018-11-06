2f33b9a1c5e7f6f78f26b3c51de02b5ed1e4176e
dinarka <dinarka.d@gmail.com>
2018-11-06 23:09:01 +0600

    Update quad_equation.rb

--- quad_equation.rb
+++ quad_equation.rb
@@ -7,13 +7,14 @@ coef_b = gets.chomp.to_f
 puts 'Введите коэффициент c:'
 coef_c = gets.chomp.to_f
 
-discr = coef_b**2 - 4*coef_a*coef_c
+discr = coef_b ** 2 - (4 * coef_a * coef_c)
 if discr > 0
-  x1 = (-coef_b + Math.sqrt(discr)) / 2*coef_a
-  x2 = (-coef_b - Math.sqrt(discr)) / 2*coef_a
+  d_sqrt = Math.sqrt(discr)
+  x1 = (-coef_b + d_sqrt) / 2 * coef_a
+  x2 = (-coef_b - d_sqrt) / 2 * coef_a
   puts "Дискриминант: #{discr} \nx1: #{x1} \nx2: #{x2}"
 elsif discr == 0
-  x = -coef_b / 2*coef_a
+  x = -coef_b / 2 * coef_a
   puts "Дискриминант: #{discr} \nx: #{x}"
 elsif  discr < 0
   puts "Дискриминант: #{discr} \nКорней нет"
