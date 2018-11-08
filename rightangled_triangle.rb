0741f08f65032fe93a60a35b0bb5603c5b7fdcb5
dinarka <dinarka.d@gmail.com>
2018-11-08 20:11:38 +0600

    Update rightangled_triangle.rb

--- rightangled_triangle.rb
+++ rightangled_triangle.rb
@@ -15,9 +15,7 @@ third_side = sorted[0]
 
 if hypotenuse == second_side ** 2 + third_side ** 2
   puts 'Треугольник прямоугольный'
-end
-
-if hypotenuse == second_side && hypotenuse == third_side
+elsif hypotenuse == second_side && hypotenuse == third_side
   puts 'Треугольник равнобедренный и равносторонний'
   puts 'Значит, не прямоугольный'
 elsif second_side == third_side || hypotenuse == second_side || hypotenuse == third_side
